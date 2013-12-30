4adc9bad65a8cf36dcc691239d94078dcd3f30a2
MauricioMurga <mauricio_murga@hotmail.com>
2013-12-30 12:48:36 -0600

    my first commit

---
+++ lib/conekta/ruby/new.rb
@@ -0,0 +1,9 @@
+require "conekta/ruby/new/version"
+
+module Conekta
+  module Ruby
+    module New
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/conekta/ruby/new/version.rb
@@ -0,0 +1,7 @@
+module Conekta
+  module Ruby
+    module New
+      VERSION = "0.0.1"
+    end
+  end
+end
