6e9d4a9e6c6accaf56257e7f557e2f9f327e9748
MauricioMurga <mauricio_murga@hotmail.com>
2014-01-01 14:45:31 -0600

    finished fixing and adding vital classes

--- lib/conekta.rb
+++ lib/conekta.rb
@@ -20,8 +20,12 @@ require "conekta/charge"
 module Conekta
   #@api_base = 'https://api.conekta.io'
   @api_base = 'http://localhost:3000'
+  @api_version = '0.3.0'
   def self.api_base
     @api_base
+  end
+  def self.api_version
+    @api_version
   end
   def self.api_key
     @api_key
--- lib/conekta/util.rb
+++ lib/conekta/util.rb
@@ -1,4 +1,3 @@
-require "active_support"
 module Conekta
   module Util
     def self.types
