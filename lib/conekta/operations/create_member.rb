34ec4d67e5580cb65cbcb5366ab8ed36dd99f08a
MauricioMurga <mauricio_murga@hotmail.com>
2014-01-09 12:18:57 -0600

    fix stack to queue

--- lib/conekta/operations/create_member.rb
+++ lib/conekta/operations/create_member.rb
@@ -7,14 +7,15 @@ module Conekta
         requestor = Requestor.new
         response = requestor.request(:post, url, params)
         if self.method(member).call and self.method(member).call.class.class_name == "ConektaObject"
-          arr = [response]
+          arr = []
           self.method(member).call.values.each do |(k,v)|
             arr << v.to_hash
           end
+          arr << response
           self.method(member).call.load_from(arr)
           self.load_from
           instances = self.method(member).call
-          instance = instances[0]
+          instance = instances.last
         else
           instance = Util.types[member.to_s].new()
           instance.load_from(response)
