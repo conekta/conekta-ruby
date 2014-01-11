53163a0aa40eace743715c5f0f5048d3c1ce4826
MauricioMurga <mauricio_murga@hotmail.com>
2014-01-10 18:36:09 -0600

    back to prod

--- lib/conekta.rb
+++ lib/conekta.rb
@@ -25,8 +25,7 @@ require "conekta/token"
 require "conekta/event"
 
 module Conekta
-#  @api_base = 'https://api.conekta.io'
-  @api_base = 'http://localhost:3000'
+  @api_base = 'https://api.conekta.io'
   @api_version = '0.3.0'
   def self.api_base
     @api_base
