28d86a31bcd4e2d9cb1c5d47a9dc8bdd4fe33692
MauricioMurga <mauricio_murga@hotmail.com>
2014-01-13 17:38:40 -0600

    deprecated methods

--- lib/conekta/operations/find.rb
+++ lib/conekta/operations/find.rb
@@ -10,6 +10,15 @@ module Conekta
           instance.load_from(response)
           instance
         end
+        # <b>DEPRECATED:</b> Please use <tt>find</tt> instead.
+        def retrieve(id)
+          instance = self.new(id)
+          url = instance.url
+          requestor = Requestor.new
+          response = requestor.request(:get, url)
+          instance.load_from(response)
+          instance
+        end
       end
       def self.included(base)
         base.extend(ClassMethods)
--- lib/conekta/operations/where.rb
+++ lib/conekta/operations/where.rb
@@ -10,6 +10,15 @@ module Conekta
           instance.load_from(response)
           instance
         end
+        # <b>DEPRECATED:</b> Please use <tt>where</tt> instead.
+        def all(params=nil)
+          instance = ConektaObject.new
+          url = Util.types[self.class_name.downcase].url
+          requestor = Requestor.new
+          response = requestor.request(:get, url)
+          instance.load_from(response)
+          instance
+        end
       end
       def self.included(base)
         base.extend(ClassMethods)
