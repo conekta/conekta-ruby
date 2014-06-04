a06d1cc32c2d6e28a17bed9f1e960b5734870964
Mackaber Witckin <mkbravo555@gmail.com>
2014-06-04 12:55:04 -0500

    Fixed Requestor and Where methods

--- lib/conekta/requestor.rb
+++ lib/conekta/requestor.rb
@@ -15,27 +15,23 @@ module Conekta
     def request(meth, url, params=nil)
       url = self.api_url(url)
       meth = meth.downcase
-      begin
-        conn = Faraday.new(:ssl => {
-            :ca_file      => File.dirname(__FILE__) + '/../ssl_data/ca_bundle.crt'
-          }
+      begin      
+        conn = Faraday.new(
+	 	:url => url,
+		:ssl => { :ca_file=> File.dirname(__FILE__) + '/../ssl_data/ca_bundle.crt'}
           ) do |faraday|
           faraday.adapter  Faraday.default_adapter
         end
-        response = conn.method(meth).call do |req|
-          req.url url
-
-          req.headers['X-Conekta-Client-User-Agent'] = set_headers.to_json
-          req.headers['User-Agent'] = 'Conekta/v1 RubyBindings/' + Conekta::VERSION
-          req.headers['Accept'] = "application/vnd.conekta-v#{Conekta.api_version}+json"
-          req.headers['Accept-Language'] = Conekta.locale
-          req.headers['Authorization'] = "Basic #{ Base64.encode64("#{self.api_key}" + ':')}"
-          if params
-            req.headers['Content-Type'] = 'application/json'
-            req.body = params.to_json
-          end
-        end
-      rescue Exception => e
+	  conn.headers['X-Conekta-Client-User-Agent'] = set_headers.to_json
+          conn.headers['User-Agent'] = 'Conekta/v1 RubyBindings/' + Conekta::VERSION
+          conn.headers['Accept'] = "application/vnd.conekta-v#{Conekta.api_version}+json"
+          conn.headers['Accept-Language'] = Conekta.locale
+          conn.headers['Authorization'] = "Basic #{ Base64.encode64("#{self.api_key}" + ':')}"
+	  if params
+	     conn.params = params
+	  end
+	  response = conn.method(meth).call
+     rescue Exception => e
         Error.error_handler(e, "")
       end
       if response.status != 200
