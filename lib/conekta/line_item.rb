4364f79a5e74e3c3d2cce6aaf5226a9418706ec4
Mauricio Murga <mauricio@conekta.io>
2016-11-25 16:32:53 -0600

    update _url method for tax_line and line_item

--- lib/conekta/line_item.rb
+++ lib/conekta/line_item.rb
@@ -5,9 +5,18 @@ module Conekta
     include Conekta::Operations::CustomAction
 
     def _url
-      raise Error.new(
-        I18n.t('error.resource.id',  { resource: self.class.class_name, locale: :en }),
-        I18n.t('error.resource.id_purchaser',  { locale: Conekta.locale.to_sym })) if (id.nil? || id.empty?)
+      if (id.nil? || id.to_s.empty?)
+        exception = Error.new({
+          "message" => I18n.t('error.resource.id',  { resource: self.class.class_name, locale: :en }),
+          "message_to_purchaser" => I18n.t('error.resource.id_purchaser',  { locale: Conekta.locale.to_sym })
+        })
+        if Conekta.api_version == "1.1.0"
+          error_list = Conekta::ErrorList.new
+          error_list.details << exception
+          exception = error_list
+        end
+        raise exception
+      end
 
       "#{self.order._url}#{self.class._url}/#{id}"
     end
