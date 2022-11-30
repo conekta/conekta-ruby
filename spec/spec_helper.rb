9f65a9c364694622eb5d6a5096177d5707ca7f9c
Fernando Sáenz <fernando.saenz@conekta.com>
2022-11-29 23:08:41 -0300

    Update README

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,6 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rspec'
 require 'pry'
 require 'conekta'
-require 'byebug'
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
