4f509df725b4932000eef2d51e50dcf70a01e5c7
Franklin <franklin.carrero@conekta.com>
2025-08-11 09:28:07 -0500

    Add OneTimeChargeBnpl class and example instructions to README

---
+++ examples/one_time_charge_bnpl.rb
@@ -0,0 +1,46 @@
+# Load the gem
+require 'conekta'
+class OneTimeChargeBnpl
+  Conekta.configure do |config|
+    config.access_token = ENV['CONEKTA_API_KEY']
+  end
+
+  api_instance = Conekta::OrdersApi.new
+  order_request = Conekta::OrderRequest.new(
+    :currency => 'MXN',
+    :line_items => [
+      {
+        :name => 'Box of Cohiba S1s',
+        :unit_price => 35000,
+        :quantity => 1
+      }
+    ],
+    :customer_info => {
+      :name => 'Mario Hernandez',
+      :email => 'mm@gmail.com',
+      :phone => '5555555555'
+    },
+    :charges => [
+      {
+        :payment_method => Conekta::PaymentMethodBnplRequest.new(
+          :expires_at => Time.now.advance(days: 30).to_i,
+          :product_type => 'creditea_bnpl',
+          :type => 'bnpl',
+          :success_url => 'https://www.example.com/success',
+          :failure_url => 'https://www.example.com/failure',
+          :cancel_url => 'https://www.example.com/cancel'
+        )
+      }
+    ]
+  )
+  opts = {
+    accept_language: 'es' # String | Use for knowing which language to use
+  }
+
+  begin
+    result = api_instance.create_order(order_request, opts)
+    p result
+  rescue Conekta::ApiError => e
+    puts "Exception when calling create order #{e}"
+  end
+end
