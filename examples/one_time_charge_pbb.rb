9673bd7fa8674337ebb3d0f842bb1f052ceaf663
Franklin <franklin.carrero@conekta.com>
2025-08-21 16:59:42 -0500

    Update documentation and add PaymentMethodPbb support

--- examples/one_time_charge_pbb.rb
+++ examples/one_time_charge_pbb.rb
@@ -12,7 +12,8 @@ class OneTimeChargePbb
       {
         :name => 'Box of Cohiba S1s',
         :unit_price => 35000,
-        :quantity => 1
+        :quantity => 1,
+        :description => 'Imported From Mex',
       }
     ],
     :customer_info => {
@@ -20,13 +21,29 @@ class OneTimeChargePbb
       :email => 'mm@gmail.com',
       :phone => '5555555555'
     },
+    :shipping_lines =>[
+    {
+        :amount => 0,
+        :carrier => 'FEDEX',
+        :method => 'FEDEX_GROUND',
+      }
+    ],
+    :shipping_contact => {
+      :address => {
+        :street1 => 'Calle 123',
+        :city => 'Mexico DF',
+        :state => 'DF',
+        :country => 'MX',
+        :postal_code => '00000'
+      },
+      :receiver => 'Mario Hernandez'
+    },
     :charges => [
       {
         :payment_method => Conekta::PaymentMethodPbbRequest.new(
-          :expires_at => Time.now.advance(days: 30).to_i,
           :product_type => 'bbva_pay_by_bank',
           :type => 'pay_by_bank',
-        )
+          )
       }
     ]
   )
@@ -38,9 +55,9 @@ class OneTimeChargePbb
     result = api_instance.create_order(order_request, opts)
     p result
     p result.id
-    p result.charges.data[0].payment_method.pay_by_bank_payment.redirect_url
-    p result.charges.data[0].payment_method.pay_by_bank_payment.deep_link
-    p result.charges.data[0].payment_method.pay_by_bank_payment.type
+    p result.charges.data[0].payment_method.redirect_url
+    p result.charges.data[0].payment_method.deep_link
+    p result.charges.data[0].payment_method.type
   rescue Conekta::ApiError => e
     puts "Exception when calling create order #{e}"
   end
